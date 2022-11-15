2f68db26f4e97869b11c63d9caf325cde1044708
Mansi Mishra <mansimishra510@gmail.com>
2022-11-15 18:34:28 +0530

    Added Box.jsx

---
+++ src/Components/Box.jsx
@@ -0,0 +1,29 @@
+
+import React from "react";
+import './assets/css/Box.css';
+import requiredThings from "./Js/requiredThings";
+
+
+function Item(props) {
+	return (
+		<div className="flex required-thing">
+			<img src={props.img} alt={props.alt} width="50" height="50"></img>
+			<p>{props.name}</p>
+		</div>
+	);
+}
+
+
+
+function Box(props){
+	return (
+		<div className="flex required-things-box">
+			{Object.keys(requiredThings[props.weather]).map(item=>{
+				return <Item img={requiredThings[props.weather][item]} name={item}/>
+			})}
+		</div>
+	)
+}
+
+
+export default Box;
