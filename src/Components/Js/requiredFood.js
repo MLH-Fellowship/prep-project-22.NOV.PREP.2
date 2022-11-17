363a92c8716b41511021c954289dd480d571691b
Ahmed Emad <ahmedemad242@gmail.com>
2022-11-17 23:47:51 +0200

    feat: add required food list

---
+++ src/Components/Js/requiredFood.js
@@ -0,0 +1,86 @@
+import * as icons from '../assets/requiredFood/icons.js';
+
+const requiredThings = {
+	Thunderstorm: {
+		Nuggets: icons.nuggets,
+		Coffee: icons.Coffee,
+		Spaghetti: icons.spaghetti,
+	},
+	Drizzle: {
+		Chocoloate: icons.Chocoloate,
+		Pancake: icons.pancake,
+		Coffee: icons.Coffee,
+	},
+	Rain: {
+		Chocoloate: icons.Chocoloate,
+		Coffee: icons.Coffee,
+		Steak: icons.steak,
+	},
+	Snow: {
+		Steak: icons.steak,
+		Chocoloate: icons.Chocoloate,
+		Coffee: icons.Coffee,
+	},
+	Mist: {
+		Burger: icons.burger,
+		Waffles: icons.waffles,
+		Coffee: icons.Coffee,
+	},
+	Smoke: {
+		Crackers: icons.cracker,
+		'Canned food': icons.cannedFood,
+		Coffee: icons.coffee,
+	},
+	Haze: {
+		Crackers: icons.cracker,
+		Coffee: icons.coffee,
+		'Canned food': icons.cannedFood,
+	},
+	Dust: {
+		Coffee: icons.coffee,
+		Crackers: icons.cracker,
+		'Canned food': icons.cannedFood,
+	},
+	Fog: {
+		Burger: icons.burger,
+		Waffles: icons.waffles,
+		Coffee: icons.Coffee,
+	},
+	Sand: {
+		Smoothie: icons.smoothie,
+		Crackers: icons.cracker,
+		'Canned food': icons.cannedFood,
+	},
+	Ash: {
+		'Canned food': icons.cannedFood,
+		Crackers: icons.cracker,
+		Coffee: icons.coffee,
+	},
+	Squall: {
+		Waffles: icons.waffles,
+		Coffee: icons.Coffee,
+		pancake: icons.pancake,
+	},
+	Tornado: {
+		Crackers: icons.cracker,
+		'Canned food': icons.cannedFood,
+		Coffee: icons.coffee,
+	},
+	Clear: {
+		Apple: icons.apple,
+		Coffee: icons.coffee,
+		Banana: icons.banana,
+	},
+	Clouds: {
+		Coffee: icons.coffee,
+		Chocoloate: icons.chocolate,
+		Pizza: icons.pizza,
+	},
+	Sunny: {
+		Lemonade: icons.lemonade,
+		'Ice cream': icons.icecream,
+		Waffles: icons.waffles,
+	},
+};
+
+export default requiredThings;
