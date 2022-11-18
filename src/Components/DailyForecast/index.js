bc3aa129fdb96d4f27260911113e057b253d013d
Cynthia Iradukunda <c.iradukund@alustudent.com>
2022-11-18 22:21:07 +0200

    fix: formating

--- src/Components/DailyForecast/index.js
+++ src/Components/DailyForecast/index.js
@@ -1,35 +1,34 @@
 import React, { useState } from 'react';
 import './index.css';
 import dateFormat from 'dateformat';
-import {FcNext} from 'react-icons/fc'; 
-import {FcPrevious} from 'react-icons/fc'
+import { FcNext } from 'react-icons/fc';
+import { FcPrevious } from 'react-icons/fc';
 const DailyForecast = ({ data, setActiveWeatherCard, activeWeatherCard }) => {
 	const initialNOfitems = () => {
-         if(window.innerWidth < 450){
-		    return 1; 
-		 }else if(window.innerWidth > 450 && window.innerWidth  < 800){
-             return 2;
-		 }else{
-			return 4; 
-		 }
-	}
-	const [startIndex, setStartIndex] = useState(0); 
-    const [endIndex, setEndIndex] = useState(initialNOfitems()); 
-    console.log(endIndex)
-	const nextWeatherCard = () =>{
-		if(endIndex < data.length-1){
-			setStartIndex(prev => prev +1); 
-			setEndIndex(prev => prev+1); 
-
+		if (window.innerWidth < 450) {
+			return 1;
+		} else if (window.innerWidth > 450 && window.innerWidth < 800) {
+			return 2;
+		} else {
+			return 4;
 		}
-	}
-
-	const prevWeatherCard = () =>{
-		if(startIndex > 0){
-			setStartIndex(prev => prev -1); 
-			setEndIndex(prev => prev -1); 
+	};
+	const [startIndex, setStartIndex] = useState(0);
+	const [endIndex, setEndIndex] = useState(initialNOfitems());
+	console.log(endIndex);
+	const nextWeatherCard = () => {
+		if (endIndex < data.length - 1) {
+			setStartIndex((prev) => prev + 1);
+			setEndIndex((prev) => prev + 1);
 		}
-	}
+	};
+
+	const prevWeatherCard = () => {
+		if (startIndex > 0) {
+			setStartIndex((prev) => prev - 1);
+			setEndIndex((prev) => prev - 1);
+		}
+	};
 	const formatDate = (date) => {
 		const currentDate = new Date(date);
 		return dateFormat(currentDate, 'ddd, mmmm dS');
@@ -37,37 +36,39 @@ const DailyForecast = ({ data, setActiveWeatherCard, activeWeatherCard }) => {
 
 	return (
 		<div className="weatherCards">
-			<FcPrevious onClick={() => prevWeatherCard()} className="icon"/>
+			<FcPrevious onClick={() => prevWeatherCard()} className="icon" />
 
 			{data &&
-				data.filter((element, index) => index >=startIndex && index  <= endIndex).map((element, index) => (
-					<div
-						id={index}
-						className={`weatherCard ${index === activeWeatherCard ? 'active' : 'deactive'} `}
-						onClick={() => setActiveWeatherCard(index)}
-					>
-						<h4>{formatDate(element.date)}</h4>
-						<div>
-							<img
-								src={`http://openweathermap.org/img/wn/${element.data[0]?.weather[0].icon}@2x.png`}
-								alt="weather icon"
-							/>
-							<h4>{element.data[0]?.main.temp} &deg;C</h4>
-						</div>
-						<p>{element.data[0]?.weather[0].main}</p>
-						<div>
+				data
+					.filter((element, index) => index >= startIndex && index <= endIndex)
+					.map((element, index) => (
+						<div
+							id={index}
+							className={`weatherCard ${index === activeWeatherCard ? 'active' : 'deactive'} `}
+							onClick={() => setActiveWeatherCard(index)}
+						>
+							<h4>{formatDate(element.date)}</h4>
 							<div>
-								<h5>Humidity</h5>
-								<p>{element.data[0]?.main.humidity}%</p>
+								<img
+									src={`http://openweathermap.org/img/wn/${element.data[0]?.weather[0].icon}@2x.png`}
+									alt="weather icon"
+								/>
+								<h4>{element.data[0]?.main.temp} &deg;C</h4>
 							</div>
+							<p>{element.data[0]?.weather[0].main}</p>
 							<div>
-								<h5>Wind Speed</h5>
-								<p>{element.data[0]?.wind.speed} km/j</p>
+								<div>
+									<h5>Humidity</h5>
+									<p>{element.data[0]?.main.humidity}%</p>
+								</div>
+								<div>
+									<h5>Wind Speed</h5>
+									<p>{element.data[0]?.wind.speed} km/j</p>
+								</div>
 							</div>
 						</div>
-					</div>
-				))}
-							<FcNext className='icon' onClick={() => nextWeatherCard()}/>
+					))}
+			<FcNext className="icon" onClick={() => nextWeatherCard()} />
 		</div>
 	);
 };
--- src/Components/MainWeatherCard/index.js
+++ src/Components/MainWeatherCard/index.js
@@ -1,48 +1,47 @@
 import './index.css';
 import dateFormat from 'dateformat';
 const MainWeatherCard = ({ data }) => {
-
 	return (
-        <div className='container'>
-		<div className="card">
-			<div>
-				<h3> Weather in {data.name}</h3>
-				<h3>{dateFormat(new Date(data.dt), 'h:MM TT')}</h3>
-			</div>
-
-			<div>
+		<div className="container">
+			<div className="card">
 				<div>
-					<div>
-						<img src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} alt="weather icon" />
-						<h3>{data.main.temp} °C</h3>
-					</div>
-					<h4>{data.weather[0].main}</h4>
-					<h5>Feels like {data.main.feels_like}</h5>
+					<h3> Weather in {data.name}</h3>
+					<h3>{dateFormat(new Date(data.dt), 'h:MM TT')}</h3>
 				</div>
 
 				<div>
 					<div>
-						<h4>Humidity</h4>
-						<h5>{data.main.humidity}%</h5>
-					</div>
-					<div>
-						<h4>Wind Speed</h4>
-						<h5>{data.wind.speed} km/j</h5>
-					</div>
-
-					<div>
-						<h4>Temp Min</h4>
-						<h5>{data.main.temp_min} °C</h5>
+						<div>
+							<img src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} alt="weather icon" />
+							<h3>{data.main.temp} °C</h3>
+						</div>
+						<h4>{data.weather[0].main}</h4>
+						<h5>Feels like {data.main.feels_like}</h5>
 					</div>
 
 					<div>
-						<h4>Temp Max</h4>
-						<h5>{data.main.temp_max} ° C</h5>
+						<div>
+							<h4>Humidity</h4>
+							<h5>{data.main.humidity}%</h5>
+						</div>
+						<div>
+							<h4>Wind Speed</h4>
+							<h5>{data.wind.speed} km/j</h5>
+						</div>
+
+						<div>
+							<h4>Temp Min</h4>
+							<h5>{data.main.temp_min} °C</h5>
+						</div>
+
+						<div>
+							<h4>Temp Max</h4>
+							<h5>{data.main.temp_max} ° C</h5>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-        </div>
 	);
 };
 
