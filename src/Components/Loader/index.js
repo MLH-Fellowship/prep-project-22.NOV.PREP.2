cb73e2d7bc3d3cff15f7b04bbee05f5291eda4f8
Cynthia Iradukunda <c.iradukund@alustudent.com>
2022-11-22 13:49:47 +0200

    fix: removed Components folder

--- src/Components/Loader/index.js
+++
@@ -1,6 +0,0 @@
-import './../../assets/css/Loader.css';
-const Loader = () => {
-	return <span className="loader"></span>;
-};
-
-export default Loader;
--- src/Components/RequiredThings/index.js
+++
@@ -1,9 +0,0 @@
-import requiredFood from './requiredFood';
-import requiredThings from './requiredThings';
-
-const getRequiredMap = {
-	food: requiredFood,
-	things: requiredThings,
-};
-
-export default getRequiredMap;
