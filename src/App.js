925282d6d7060a6e877ab4ac223bb18778f7a5f4
Cynthia Iradukunda <c.iradukund@alustudent.com>
2022-11-23 16:57:31 +0200

    fix: formatting

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import PlaylistRecommendation from './components/PlaylistRecommendation';
 
 function App() {
 	const [city, setCity] = useState('New York City');
-	const [weatherType, setWeatherType] = useState("");
+	const [weatherType, setWeatherType] = useState('');
 	const [isLoaded, setIsLoaded] = useState(false);
 	const [error, setError] = useState(null);
 	const [results, setResults] = useState(null);
@@ -51,52 +51,50 @@ function App() {
 		}
 	};
 	useEffect(() => {
-    fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-        city +
-        "&units=metric&appid=" +
-        process.env.REACT_APP_APIKEY
-    )
-      .then(res => res.json())
-      .then(
-        result => {
-          console.log(result);
+		fetch(
+			'https://api.openweathermap.org/data/2.5/weather?q=' +
+				city +
+				'&units=metric&appid=' +
+				process.env.REACT_APP_APIKEY,
+		)
+			.then((res) => res.json())
+			.then(
+				(result) => {
+					console.log(result);
 
-          if (result["cod"] !== 200) {
-            setIsLoaded(false);
-            setError(result);
-          } else {
-            setIsLoaded(true);
-            setResults(result);
-            setWeatherType(result.weather[0].main);
-            
-          }
-        },
-        error => {
-          setIsLoaded(false);
-          setError(error);
-          setWeatherType(error);
-        }
-      );
-  }, [city]);
+					if (result['cod'] !== 200) {
+						setIsLoaded(false);
+						setError(result);
+					} else {
+						setIsLoaded(true);
+						setResults(result);
+						setWeatherType(result.weather[0].main);
+					}
+				},
+				(error) => {
+					setIsLoaded(false);
+					setError(error);
+					setWeatherType(error);
+				},
+			);
+	}, [city]);
 
-  
-  const weather = (weatherType) => {
-	switch (weatherType) {
-	  case "Rain":
-		return "rainy"
-	  case "Clouds":
-		return "cloudy"
-	  case "Snow":
-		return "snowy"
-	  case "Clear":
-		return "clear"
-	  case "Haze":
-		return "haze"
-	  default:
-		return "default"
-	}
-  };
+	const weather = (weatherType) => {
+		switch (weatherType) {
+			case 'Rain':
+				return 'rainy';
+			case 'Clouds':
+				return 'cloudy';
+			case 'Snow':
+				return 'snowy';
+			case 'Clear':
+				return 'clear';
+			case 'Haze':
+				return 'haze';
+			default:
+				return 'default';
+		}
+	};
 	const findLocation = () => {
 		navigator.geolocation.getCurrentPosition((position) => {
 			setCWeatherUrl(
@@ -162,7 +160,7 @@ function App() {
 	}, [forecastData]);
 
 	if (cWeatherError || forecastError) {
-		return <div >Error: {cWeatherError.message || forecastError.message}</div>;
+		return <div>Error: {cWeatherError.message || forecastError.message}</div>;
 	} else if (cWeatherLoading || forecastLoading || cWeatherData == null || forecastData == null) {
 		return (
 			<div id="loader">
