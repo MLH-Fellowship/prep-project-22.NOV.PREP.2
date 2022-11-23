4bb706591af3b78b84aefab5d1069f6801eb8fc3
miador <yusufkaradag1402@gmail.com>
2022-11-23 16:55:01 +0100

    fix: small fix on footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,7 +6,7 @@ const Footer = () => {
 	return (
 		<footer bgColor="light" className="text-center text-lg-left">
 			<div className="content">
-				<a className="footer-text">Made with ❤️ by</a>
+				<p className="footer-text">Made with ❤️ by</p>
 				<img src={logofooter} className="logofooter" alt="logo" />
 			</div>
 		</footer>
